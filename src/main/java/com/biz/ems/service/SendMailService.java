a02bb7edb68a7d79baa42e26832da48da2cbc202
Morishita1 <iakwpi@naver.com>
2019-08-09 16:34:12 +0900

    2019-08-09

--- src/main/java/com/biz/ems/service/SendMailService.java
+++ src/main/java/com/biz/ems/service/SendMailService.java
@@ -1,6 +1,7 @@
 package com.biz.ems.service;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -30,10 +31,13 @@ public class SendMailService {
 		
 		
 		String from_email = emailVO.getEms_from_email();
+		String from_name = emailVO.getEms_from_name();
 		String to_email = emailVO.getEms_to_email();
 		String subject = emailVO.getEms_subject();
 		String content = emailVO.getEms_content();
+		
 		String file1 = emailVO.getEms_file1();
+		String file2 = emailVO.getEms_file2();
 		// Email에 사용되는 문서구조 : MimeMessage 라고한다
 		MimeMessage message = xMail.createMimeMessage();
 		
@@ -41,16 +45,42 @@ public class SendMailService {
 		MimeMessageHelper mHelper;
 		
 		try {
+			// MimeMessageHelper를 만들때
+			// 2번째 항목을 true 하면 메일에 파일을 첨부할 수있고,
+			//              false로 설정하면 순수한 text만 전송이된다.
 			mHelper = new MimeMessageHelper(message,true,"UTF-8");
-			mHelper.setFrom(from_email);
+			mHelper.setFrom(from_email,from_name);
 			mHelper.setTo(to_email);
 			mHelper.setSubject(subject);
-			mHelper.setText(content);
 			
-			FileSystemResource fr = new FileSystemResource(new File(uploadFolder,file1));
-			mHelper.addAttachment(file1, fr);
+			//2번째 항목을
+			// true 하면 HTML 방식으로 메일보내기
+			// 생략하거나 false 하면 text 방식으로 메일 보내기
+			mHelper.setText(content,true);
+			
+			FileSystemResource fr= null;
+			if(!file1.isEmpty()) {
+				// 서버의 저장소에 저장된 파일을
+				// 메일에 첨부할 수 있도록 변환하는 과정
+				fr = new FileSystemResource(new File(uploadFolder,file1));
+				
+				// fr 파일리소스를 file1의 이름으로 첨부하겠다
+				mHelper.addAttachment(file1, fr);	
+			}
+			
+			if(!file2.isEmpty()) {
+				// 서버의 저장소에 저장된 파일을
+				// 메일에 첨부할 수 있도록 변환하는 과정
+				fr = new FileSystemResource(new File(uploadFolder,file2));
+				
+				// fr 파일리소스를 file1의 이름으로 첨부하겠다
+				mHelper.addAttachment(file2, fr);	
+			}
 			xMail.send(message);
 		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
